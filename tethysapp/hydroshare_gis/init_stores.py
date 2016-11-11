4bcf2cef328fb58e46b6e439f823f69b27746c2d
shawncrawley <shawncrawley@gmail.com>
2016-11-11 09:40:21 -0700

    purge geoserver workspace on first time init_stores

--- tethysapp/hydroshare_gis/init_stores.py
+++ tethysapp/hydroshare_gis/init_stores.py
@@ -1,4 +1,12 @@
 from .model import engine, Base
+from tethys_sdk.services import get_spatial_dataset_engine
+from utilities import get_workspace
 
 def init_hydroshare_gis_layers_db(first_time):
     Base.metadata.create_all(engine)
+
+    if first_time:
+        spatial_dataset_engine = get_spatial_dataset_engine(name='default')
+        spatial_dataset_engine.delete_workspace(workspace_id=get_workspace(),
+                                                purge=True,
+                                                recurse=True)
